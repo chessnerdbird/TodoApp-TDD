5ff8b242c67e643ef2a8314ad3658a25c9b6cc24
chessnerdbird <dblackw2@gmail.com>
2018-03-07 20:32:25 -0500

    associate user to factory tasks

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,15 +1,18 @@
 FactoryBot.define do
   factory :homework, class: Task do
+  	association :user
     name "complete homework"
     priority 1
   end
 
   factory :email, class: Task do 
+  	association :user
   	name "reply to Zack's email"
   	priority 2
   end
 
   factory :invalid_task, class: Task do
+  	association :user
   	name nil
   	priority nil
   end
