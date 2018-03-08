cce81091e1546fb299af228d604ddd74ff1c4dda
chessnerdbird <dblackw2@gmail.com>
2018-03-07 20:29:48 -0500

    pass firstname lastname email tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,11 +4,24 @@ RSpec.describe User, type: :model do
 
   let(:user) { build(:user) }
 
-  it 'is invalid without firstname'
+  it 'is invalid without firstname' do
+  	user.firstname = nil
+  	expect(user).not_to be_valid
+  end
 
-  it 'is invalid without lastname'
+  it 'is invalid without lastname' do 
+	user.lastname = nil
+  	expect(user).not_to be_valid
+  end
 
-  it 'is invalid without email'
+  it 'is invalid without email' do 
+  	user.save
+  	other_user = build(:user, email: user.email)
+
+  	# expect(other_user.email).not_to eq(user.email)
+  	other_user.valid?
+  	expect(other_user.errors[:email]).to include('has already been taken')
+  end
 
   it 'is invalid without unique email'
 
