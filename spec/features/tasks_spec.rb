42ef676f36c09560988a4033da3206892ad439d4
chessnerdbird <dblackw2@gmail.com>
2018-03-14 13:28:20 -0400

    created new feature 'New Task' test, fails currently

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -11,3 +11,19 @@ feature "tasks/index"  do
 		expect(page).to have_content("reply to Zack's email")
 	end
 end
+
+feature 'New Task' do 
+	scenario 'user adds a new task' do
+		user = create(:user)
+		visit tasks_path
+
+		expect{
+			clink_link 'New Task'
+			fill_in 'Name', with: 'Learn Rspec'
+			fill_in 'Priority', with: 1
+			fill_in 'Due date', with: DateTime.now
+			select(user.email, from: 'task_user_id')
+			click_button('Create Task')
+		}.to change(Task, :count).by(1)
+	end
+end
